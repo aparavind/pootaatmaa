94261730b90951add9d5d1d5cfe5b434f5c51c1f
admin <admin@192.168.1.4>
2016-03-31 07:45:39 +0530

    completing mysqli connecton Morning - - Day20160330

--- initialize.sql
+++ initialize.sql
@@ -4,5 +4,29 @@ create table db_language_master (
     language varchar(255) not null unique key
 );
 
+/* shelf master */
+create table db_shelf_master (
+    shelfid int(11) not null auto_increment primary key,
+    shelf varchar(255) not null unique key,
+    shelf_address varchar(500)
+);
 
+/* author master */
+create table author_master (
+    authorid int(11) not null auto_increment primary key,
+    author varchar(255) not null unique key,
+    author_application varchar(500)
+);
 
+/* Book Master */
+create table book_master (
+    bookid int(11) not null auto_increment primary key,
+    book varchar(255) not null unique key,
+    authorid int(11) , 
+    shelfid int(11) ,
+    len int(11),
+    brd int(11),
+    wdh int(11),
+    foreign key (authorid) references author_master (authorid) on delete set null,
+    foreign key (shelfid) references shelf_master (shelfid)  on delete set null
+);  
